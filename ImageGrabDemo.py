fb12e8b542ba4f9c56ba6e8d456f381d06406a52
Steve Carter (from Linux) <steve.carter@rnq.com.au>
2020-11-06 10:15:00 +1000

    updates demo to use new functions (genicam_worker) in the back end

--- ImageGrabDemo.py
+++ ImageGrabDemo.py
@@ -1,8 +1,8 @@
 import cv2
 import linuxCamsApi
 
-sensor_width = 1280
-sensor_height = 1024
+sensor_width = 1280  # this is the actual sensor width not the image_width
+sensor_height = 1024 # this is the actual sensor height not the image_height
 # Ideally, you should select PAL or NTSC supported resolutions
 # others work, but its a bit hit and miss
 img_width = 1280
@@ -12,30 +12,35 @@ offset_y = 0
 channels = 3
 # this is here to show that you probably should set the framerate of the camera and the framerate
 # of the cv2 window the same.  I had problems here and this appeared to resolve them
-
-# ToDo:  make changes here so you can set more settings
 target_framerate = 144
 framerate_cv2_window = int(1000/target_framerate)
 camera = linuxCamsApi.Camera(sensor_width, sensor_height, img_width, img_height, channels)
-pcamera = camera.create_camera_instance()
+# when you create an instance of the camera, it should do the following:
+# gets camera instance
+# gets the genicam schema file - you cannot get the file without connectin to the camera first
+# unzips it
+# gets the unzipped file and loads it as a variable
+
 print("setting some properties")
 # the offset needs to be considered with setting the Width and height
 # camera.property_set(setting="DeviceReset", option='Execute', featuretype='Command')
 # its probably better to instantiate the camera pointer once in the linuxCamsApi.....
-camera.set(_property="DeviceUserID", _value='mycamera', _type='String')
-camera.set(_property="OffsetX", _value=offset_x, _type='Integer')
-camera.set(_property="OffsetY", _value=offset_y, _type='Integer')
-camera.set(_property="Width", _value=img_width, _type='Integer')
-camera.set(_property="Height", _value=img_height, _type='Integer')
-camera.set(_property="TriggerSource", _value='Software', _type='Enumeration')
-camera.set(_property="TriggerSelector", _value='FrameStart', _type='Enumeration')
-camera.set(_property='AcquisitionMode', _value='Continuous', _type='Enumeration')
+camera.property_set("DeviceUserID", 'WASSUP')
+input("confirm property set")
+camera.property_set("OffsetX", offset_x)
+camera.property_set("OffsetY", offset_y)
+camera.property_set("Width", img_width)
+camera.property_set("Height", img_height)
+camera.property_set("TriggerSource", "Software")
+camera.property_set("TriggerSelector", "FrameStart")
+camera.property_set("AcquisitionMode", "Continuous")
 # activates the camera - light should start flashing on the back
-camera.set(_property="TriggerMode", _value='On', _type='Enumeration')
+camera.property_set("TriggerMode", "On")
 camera.activate()
-camera.set(_property="ExposureAuto", _value='Off', _type='Enumeration')
-camera.set(_property="ExposureTime", _value=15000, _type='Float')  # will fail if ExposureAuto is not set to Off First
-camera.set(_property="DeviceReset", _value='Execute', _type='Command')
+camera.property_set("ExposureAuto", "Off")
+camera.property_set("ExposureTime", 15000)
+#camera.set(_property="ExposureTime", _value=15000, _type='Float')  # will fail if ExposureAuto is not set to Off First
+# camera.set(_property="DeviceReset", _value='Execute', _type='Command')
 
 print("done!")
 
@@ -44,6 +49,7 @@ while True:
     corrected_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     cv2.imshow('Contrastech Mars USB3 Vision Camera Test', corrected_image)
     k = cv2.waitKey(framerate_cv2_window)
+    camera.property_get_temp()
     #print(k)
     if k == 1048603:  # Esc key to stop
         # camera.change_setting(setting=b"TriggerMode", option=b'Off')
